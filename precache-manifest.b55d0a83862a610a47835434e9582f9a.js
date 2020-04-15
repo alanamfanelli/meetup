ee85f39813456430b27341ad402d10fdbcb351cb
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-04-15 10:28:52 -0600

    Updates

---
+++ precache-manifest.b55d0a83862a610a47835434e9582f9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "737a63b2f9e7c413498f",
+    "url": "/meetup/static/css/main.503a147f.chunk.css"
+  },
+  {
+    "revision": "737a63b2f9e7c413498f",
+    "url": "/meetup/static/js/main.757fabb6.chunk.js"
+  },
+  {
+    "revision": "c81de03e5fcc2eb84eed",
+    "url": "/meetup/static/js/runtime~main.25e5dfeb.js"
+  },
+  {
+    "revision": "881f4e804bdbebdbf086",
+    "url": "/meetup/static/js/2.ad3a10a5.chunk.js"
+  },
+  {
+    "revision": "351de3487a297743ea35f411b2a54fd7",
+    "url": "/meetup/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.ab400aa23688c79ae88cea85b99a6351.js"
+  "/meetup/precache-manifest.b55d0a83862a610a47835434e9582f9a.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meetup/index.html"), {
+workbox.routing.registerNavigationRoute("/meetup/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
